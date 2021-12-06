bfe12c570007ff18e797afb7227e91cb5af0e5e4
Christoph Neumüller <c.neumueller@gmail.com>
2021-12-06 09:22:42 +0100

    6 part 2

--- 2021/06/Program.cs
+++ 2021/06/Program.cs
@@ -1,26 +1,32 @@
 ﻿using utils;
 
-// part1
-{
-    var r = new MyReader(File.OpenText("large.txt"));
-    using var stopwatch = AutoStopwatch.Start();
+var r = new MyReader(File.OpenText("large.txt"));
+using var stopwatch = AutoStopwatch.Start();
 
-    var fish = new List<int>();
-    while (!r.EOF) fish.Add(r.ReadInt());
+var fish = new Dictionary<long, long>();
+while (!r.EOF) AddVal(fish, r.ReadInt(), 1);
 
-    int days = 80;
-    for (int i = 0; i < days; i++) {
-        var newfish = new List<int>();
-        foreach (var f in fish) {
-            if (f == 0) {
-                newfish.Add(6);
-                newfish.Add(8);
-            } else {
-                newfish.Add(f - 1);
-            }
+int days = 256;
+for (int i = 0; i < days; i++) {
+    var newfish = new Dictionary<long, long>();
+    foreach (var f in fish) {
+        if (f.Value == 0) continue;
+        if (f.Key == 0) {
+            AddVal(newfish, 6, f.Value);
+            AddVal(newfish, 8, f.Value);
+        } else {
+            AddVal(newfish, f.Key - 1, f.Value);
         }
-        fish = newfish;
     }
+    fish = newfish;
+}
 
-    Console.WriteLine(fish.Count());
+Console.WriteLine(fish.Values.Sum());
+
+void AddVal(Dictionary<long, long> dict, long key, long val) {
+    if (!dict.ContainsKey(key)) {
+        dict.Add(key, val);
+    } else {
+        dict[key] += val;
+    }
 }

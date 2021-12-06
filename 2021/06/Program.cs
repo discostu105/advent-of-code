637d2c6033560dc2b9e58010a114e47a7e940891
Christoph Neumüller <c.neumueller@gmail.com>
2021-12-06 08:25:41 +0100

    06 part 1

---
+++ 2021/06/Program.cs
@@ -0,0 +1,26 @@
+﻿using utils;
+
+// part1
+{
+    var r = new MyReader(File.OpenText("large.txt"));
+    using var stopwatch = AutoStopwatch.Start();
+
+    var fish = new List<int>();
+    while (!r.EOF) fish.Add(r.ReadInt());
+
+    int days = 80;
+    for (int i = 0; i < days; i++) {
+        var newfish = new List<int>();
+        foreach (var f in fish) {
+            if (f == 0) {
+                newfish.Add(6);
+                newfish.Add(8);
+            } else {
+                newfish.Add(f - 1);
+            }
+        }
+        fish = newfish;
+    }
+
+    Console.WriteLine(fish.Count());
+}

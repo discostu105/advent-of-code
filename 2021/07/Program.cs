f72fee509b894ff1f1e89d944fd95dfef755a605
Christoph Neumüller <c.neumueller@gmail.com>
2021-12-07 07:51:42 +0100

    07

---
+++ 2021/07/Program.cs
@@ -0,0 +1,33 @@
+﻿using utils;
+
+var r = new MyReader(File.OpenText("large.txt"));
+using var stopwatch = AutoStopwatch.Start();
+
+var positions = new List<int>();
+while (!r.EOF) positions.Add(r.ReadInt());
+
+var minDistance = int.MaxValue;
+var idealPos = -1;
+for (int pos = 0; pos < positions.Count; pos++) {
+    var distance = positions.Sum(p => FuelCost(p, pos));
+    if (distance < minDistance) {
+        minDistance = distance;
+        idealPos = pos;
+    }
+}
+
+Console.WriteLine($"idealPos: {idealPos}");
+Console.WriteLine($"minDistance: {minDistance}");
+
+int FuelCost(int p, int pos) {
+    var distance = Math.Abs(p - pos);
+    // part1
+    // return distance;
+
+    // part2
+    int sum = 0;
+    for (int i = 0; i < distance; i++) {
+        sum += i + 1;
+    }
+    return sum;
+}

10cf9c30518b7f473514ffefddcfe839b4aebd55
Christoph Neumueller <christoph.neumueller@dynatrace.com>
2022-12-01 08:13:01 +0100

    01 part 1

---
+++ 2022/01/Program.cs
@@ -0,0 +1,19 @@
+﻿using utils;
+
+var r = new MyReader(File.OpenText("small.txt"));
+
+var maxCalories = 0;
+while (!r.EOF)
+{
+    var calories = 0;
+    while (!r.EOL)
+    {
+        calories += r.ReadInt();
+    }
+    r.SkipEOL();
+
+    Console.WriteLine("new elf: " + calories);
+    maxCalories = Math.Max(maxCalories, calories);
+}
+
+Console.WriteLine("max calories: " + maxCalories);
---
+++ 2022/utils/AutoStopwatch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace utils;
+
+public class AutoStopwatch : IDisposable {
+    private readonly Stopwatch stopwatch;
+
+    public AutoStopwatch() {
+        this.stopwatch = Stopwatch.StartNew();
+    }
+
+    public void Dispose() {
+        Console.WriteLine($"Elapsed time: {stopwatch.Elapsed}");
+    }
+
+    public static AutoStopwatch Start() {
+        return new AutoStopwatch();
+    }
+}
+

a3f43134f7d845effeb75bd500fc32c2f6631b9a
Christoph Neumueller <christoph.neumueller@dynatrace.com>
2022-12-01 08:26:00 +0100

    tweaks

--- 2022/01/Program.cs
+++ 2022/01/Program.cs
@@ -1,10 +1,8 @@
-﻿using System.Linq;
-using utils;
+﻿using utils;
 
 var r = new MyReader(File.OpenText("small.txt"));
 
-var maxCalories = 0;
-var elfes = new SortedList<int, int>();
+var elves = new SortedList<int, int>();
 while (!r.EOF)
 {
     var calories = 0;
@@ -13,8 +11,8 @@ while (!r.EOF)
         calories += r.ReadInt();
     }
     r.SkipEOL();
-    elfes.Add(calories, calories);
-    maxCalories = Math.Max(maxCalories, calories);
+    elves.Add(calories, calories);
 }
 
-Console.WriteLine("max calories: " + (elfes.TakeLast(3).Sum(x => x.Value)));
+Console.WriteLine("top 1 elve calories: " + (elves.TakeLast(1).Sum(x => x.Value)));
+Console.WriteLine("top 3 elve calories: " + (elves.TakeLast(3).Sum(x => x.Value)));

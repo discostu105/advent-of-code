d1c13e175ffce9c160c4fed4805d52798a0db37e
Christoph Neumüller <c.neumueller@gmail.com>
2022-12-03 21:24:28 +0100

    beautify

--- 2022/03/Program.cs
+++ 2022/03/Program.cs
@@ -2,43 +2,46 @@
 
 // part 1
 {
-    using var r = new MyReader(File.OpenText("test.txt"));
-    var sum = 0;
+    int sum = 0;
+    using var r = new MyReader(File.OpenText("input.txt"));
     while (!r.EOF) {
-        string line = r.ReadLine();
-        int compartmentSize = line.Length / 2;
-        var compartment1 = line.Substring(0, compartmentSize);
-        var compartment2 = line.Substring(compartmentSize);
-
-        char match = '_';
-        foreach (var c in compartment1) {
-            if (compartment2.Contains(c)) match = c;
-        }
-        sum += Priority(match);
+        var line = r.ReadLine();
+        sum += Priority(FindCommonCharacter(new string[] { 
+            line[..(line.Length / 2)], 
+            line[(line.Length / 2)..]}
+        ));
     }
-    Console.WriteLine("part 1 solution:" + sum);
+    Console.WriteLine("part 1 solution: " + sum);
 }
 
 // part 2
 {
-    using var r = new MyReader(File.OpenText("input.txt"));
-
     var n = 3;
     var groups = new string[n];
     int sum = 0;
+
+    using var r = new MyReader(File.OpenText("input.txt"));
     while (!r.EOF) {
         for (int i = 0; i < n; i++) groups[i] = r.ReadLine();
-        char badge = '_';
-        foreach(var c in groups[0]) {
-            bool match = true;
-            for (int i = 1; i < n; i++) {
-                if (!groups[i].Contains(c)) match = false;
-            }
-            if (match) badge = c;
-        }
-        sum += Priority(badge);
+        sum += Priority(FindCommonCharacter(groups));
     }
     Console.WriteLine("part 2 solution: " + sum);
+}
+
+static char FindCommonCharacter(string[] groups) {
+    char badge = default(char);
+    foreach (var c in groups[0]) {
+        bool match = true;
+        for (int i = 1; i < groups.Length; i++) {
+            if (!groups[i].Contains(c)) match = false;
+        }
+        if (match && badge != c) {
+            if (badge != default(char)) throw new Exception("multiple common characters found");
+            badge = c;
+        }
+    }
+    if (badge == default(char)) throw new Exception("no common character found");
+    return badge;
 }
 
 int Priority(char c) {

e2e36a0228e45aa8fb5a80c144786bddc54af589
Christoph Neumüller <c.neumueller@gmail.com>
2022-12-05 17:38:47 +0100

    05 part 1

---
+++ 2022/05/Program.cs
@@ -0,0 +1,64 @@
+﻿using utils;
+
+using var r = new MyReader(File.OpenText("input.txt"), new char[] { '[', ']', ' ' });
+
+int stackcount = -1;
+Stack<char>[] stacks = null;
+
+while (!r.EOF) {
+    var line = r.ReadLine();
+    if (stackcount < 0) stackcount = (line.Length + 1) / 4;
+    if (stacks == null) stacks = new Stack<char>[stackcount];
+    if (line.Contains('[')) {
+        // stack content
+        for (int i = 0; i < stackcount; i++) {
+            var crate = line[i * 4 + 1];
+            if (stacks[i] == null) stacks[i] = new Stack<char>();
+            if (crate != ' ') stacks[i].Push(crate);
+            //Console.WriteLine(i + ": " + crate);
+        }
+    } else if (line.StartsWith("move")) {
+        // move command
+        using var linereader = new MyReader(GenerateStreamFromString(line));
+
+        linereader.ReadWord();
+        var count = linereader.ReadInt();
+        linereader.ReadWord();
+        var from = linereader.ReadInt();
+        linereader.ReadWord();
+        var to = linereader.ReadInt();
+
+        for (int i = 0; i < count; i++) {
+            var crate = stacks[from - 1].Pop();
+            stacks[to - 1].Push(crate);
+        }
+    } else {
+        // skip
+        
+        for (int i = 0; i < stackcount; i++) {
+            stacks[i] = Reverse(stacks[i]);
+        }
+    }
+}
+
+foreach(var stack in stacks) {
+    Console.Write(stack.Peek());
+}
+
+static StreamReader GenerateStreamFromString(string s) {
+    var stream = new MemoryStream();
+    var writer = new StreamWriter(stream);
+    writer.Write(s);
+    writer.Flush();
+    stream.Position = 0;
+    return new StreamReader(stream);
+}
+
+static Stack<char> Reverse(Stack<char> input) {
+    var temp = new Stack<char>();
+
+    while (input.Count != 0)
+        temp.Push(input.Pop());
+
+    return temp;
+}
--- 2022/utils/MyReader.cs
+++ 2022/utils/MyReader.cs
@@ -3,173 +3,182 @@
 namespace utils;
 
 public class MyReader : IDisposable {
-	private StreamReader sr;
+    private StreamReader sr;
 
-	public MyReader(StreamReader sr, char[] separators = null) {
-		this.sr = sr;
-		if (separators != null)
-			this.Separators = separators;
-		else
-			this.Separators = new char[] { ' ', ',' };
-	}
+    public MyReader(StreamReader sr, char[] separators = null) {
+        this.sr = sr;
+        if (separators != null)
+            this.Separators = separators;
+        else
+            this.Separators = new char[] { ' ', ',' };
+    }
 
-	public string ReadWord() {
-		SkipSeparators();
-		eol = false;
-		var sb = new StringBuilder();
-		char c;
-		while (!sr.EndOfStream) {
-			c = (char)sr.Read();
-			if (Separators.Contains(c)) break;
-			if (IsNewLine(c)) {
-				eol = true;
-				//char nextch = (char)sr.Peek();
-				//while (IsNewLine(nextch)) {
-				//	sr.Read(); // consume all newlines
-				//	nextch = (char)sr.Peek();
-				//}
-				SkipEOL(); // skip 1x newline
-				break;
-			}
-			sb.Append(c);
-		}
-		return sb.ToString();
-	}
+    public string ReadWord() {
+        SkipSeparators();
+        eol = false;
+        var sb = new StringBuilder();
+        char c;
+        while (!sr.EndOfStream) {
+            c = (char)sr.Read();
+            if (Separators.Contains(c)) break;
+            if (IsNewLine(c)) {
+                eol = true;
+                //char nextch = (char)sr.Peek();
+                //while (IsNewLine(nextch)) {
+                //	sr.Read(); // consume all newlines
+                //	nextch = (char)sr.Peek();
+                //}
+                SkipEOL(); // skip 1x newline
+                break;
+            }
+            sb.Append(c);
+        }
+        return sb.ToString();
+    }
 
-	// skip over some text. do not include separators in word
+    // skip over some text. do not include separators in word
     public void Skip(string word) {
-		SkipSeparators();
-		int i = 0;
-		while (!sr.EndOfStream) {
-			sr.Read();
-			i++;
-			if (i == word.Length) return;
-		}
-	}
+        SkipSeparators();
+        int i = 0;
+        while (!sr.EndOfStream) {
+            sr.Read();
+            i++;
+            if (i == word.Length) return;
+        }
+    }
 
     private void SkipSeparators() {
-		char nextch = (char)sr.Peek();
-		while (Separators.Contains(nextch)) {
-			sr.Read(); // consume all separators
-			nextch = (char)sr.Peek();
-		}
-	}
+        char nextch = (char)sr.Peek();
+        while (Separators.Contains(nextch)) {
+            sr.Read(); // consume all separators
+            nextch = (char)sr.Peek();
+        }
+    }
 
     private bool IsNewLine(char c) {
-		return c == '\r' || c == '\n';
-	}
+        return c == '\r' || c == '\n';
+    }
 
-	public int ReadInt() {
-		string w = ReadWord();
-		return int.Parse(w);
-	}
+    public int ReadInt() {
+        string w = ReadWord();
+        return int.Parse(w);
+    }
 
-	public double ReadDouble() {
-		return double.Parse(ReadWord());
-	}
+    public double ReadDouble() {
+        return double.Parse(ReadWord());
+    }
 
-	public string ReadLine() {
-		eol = false;
-		var sb = new StringBuilder();
-		char c;
-		while (!sr.EndOfStream) {
-			c = (char)sr.Read();
-			if (IsNewLine(c)) {
-				eol = true;
-				char nextch = (char)sr.Peek();
-				while (IsNewLine(nextch)) {
-					sr.Read(); // consume all newlines
-					nextch = (char)sr.Peek();
-				}
-				break;
-			}
+    public char ReadChar() {
+        var c = (char)sr.Read();
+        char nextch = (char)sr.Peek();
+        if (IsNewLine(nextch)) eol = true;
+        return c;
+    }
+
+    public string ReadLine() {
+        eol = false;
+        var sb = new StringBuilder();
+        char c;
+        while (!sr.EndOfStream) {
+            c = (char)sr.Read();
+            if (IsNewLine(c)) {
+                eol = true;
+                char nextch = (char)sr.Peek();
+                while (IsNewLine(nextch)) {
+                    sr.Read(); // consume all newlines
+                    nextch = (char)sr.Peek();
+                }
+                break;
+            }
             sb.Append(c);
-		}
-		return sb.ToString();
-	}
+        }
+        return sb.ToString();
+    }
 
-	public bool EOF {
-		get { return sr.EndOfStream; }
-	}
+    public bool EOF {
+        get { return sr.EndOfStream; }
+    }
 
-	// skips end-of-line characters, once. considers CRLF and LF eols.
-	public void SkipEOL()
-	{
-        if (EOL)
-		{
+    // skips end-of-line characters, once. considers CRLF and LF eols.
+    public void SkipEOL() {
+        if (EOL) {
             char nextch = (char)sr.Peek();
-            if (nextch == '\r')
-            {
+            if (nextch == '\r') {
                 sr.Read();
                 nextch = (char)sr.Peek();
             }
-            if (nextch == '\n')
-            {
+            if (nextch == '\n') {
                 sr.Read();
                 nextch = (char)sr.Peek();
             }
             eol = (nextch == '\r' || nextch == '\n'); // it's ok if next line is EOL again
         }
-	}
+    }
 
 
     public char[] Separators { get; set; }
 
-	bool eol;
-	public bool EOL {
-		get { return eol || sr.EndOfStream; }
-	}
+    bool eol;
+    public bool EOL {
+        get { return eol || sr.EndOfStream; }
+    }
 
-	public T ReadObject<T>() where T : IParsable, new() {
-		var obj = new T();
-		obj.Parse(this);
-		return obj;
-	}
+    public T ReadObject<T>() where T : IParsable, new() {
+        var obj = new T();
+        obj.Parse(this);
+        return obj;
+    }
 
-	public int[] ReadIntArray() {
-		int size = ReadInt();
-		var a = new int[size];
-		for (int i = 0; i < size; i++) {
-			a[i] = ReadInt();
-		}
-		return a;
-	}
+    public int[] ReadIntArray() {
+        int size = ReadInt();
+        var a = new int[size];
+        for (int i = 0; i < size; i++) {
+            a[i] = ReadInt();
+        }
+        return a;
+    }
 
-	public double[] ReadDoubleArray() {
-		int size = ReadInt();
-		var a = new double[size];
-		for (int i = 0; i < size; i++) {
-			a[i] = ReadDouble();
-		}
-		return a;
-	}
+    public double[] ReadDoubleArray() {
+        int size = ReadInt();
+        var a = new double[size];
+        for (int i = 0; i < size; i++) {
+            a[i] = ReadDouble();
+        }
+        return a;
+    }
 
-	public string[] ReadWordArray() {
-		int size = ReadInt();
-		var a = new string[size];
-		for (int i = 0; i < size; i++) {
-			a[i] = ReadWord();
-		}
-		return a;
-	}
+    public string[] ReadWordArray() {
+        int size = ReadInt();
+        var a = new string[size];
+        for (int i = 0; i < size; i++) {
+            a[i] = ReadWord();
+        }
+        return a;
+    }
 
-	public T[] ReadObjectArray<T>() where T : IParsable, new() {
-		int size = ReadInt();
-		var a = new T[size];
-		for (int i = 0; i < size; i++) {
-			a[i] = ReadObject<T>();
-		}
-		return a;
-	}
+    public T[] ReadObjectArray<T>() where T : IParsable, new() {
+        int size = ReadInt();
+        var a = new T[size];
+        for (int i = 0; i < size; i++) {
+            a[i] = ReadObject<T>();
+        }
+        return a;
+    }
 
-	internal void NextLine() {
-		eol = false;
-	}
+    internal void NextLine() {
+        eol = false;
+    }
 
-	public void Dispose() {
-		sr.Dispose();
-	}
+    public void Dispose() {
+        sr.Dispose();
+    }
+
+    public void SkipChars(int chars) {
+        for (int i = 0; i < chars; i++) {
+            sr.Read();
+        }
+    }
 }
 public interface IParsable {
-	void Parse(MyReader r);
+    void Parse(MyReader r);
 }

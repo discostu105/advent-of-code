3d1d1f2d4db135090844b80b2278c8bf2e4af5c6
Christoph Neumüller <c.neumueller@gmail.com>
2022-12-06 20:21:48 +0100

    beautify

--- 2022/06/Program.cs
+++ 2022/06/Program.cs
@@ -1,26 +1,25 @@
 ﻿using utils;
 
-using var r = new MyReader(File.OpenText("test.txt"));
+using var r = new MyReader(File.OpenText("input.txt"));
 
 while (!r.EOF) {
     var line = r.ReadLine();
-	Console.WriteLine("part 1: " + DuplicateIndex(line, 4));
-    Console.WriteLine("part 2: " + DuplicateIndex(line, 14));
+    Console.WriteLine("part 1: " + FindNonDuplicateIndex(line, 4));
+    Console.WriteLine("part 2: " + FindNonDuplicateIndex(line, 14));
 }
 
-int DuplicateIndex(string s, int buffersize) {
+int FindNonDuplicateIndex(string s, int buffersize) {
     for (int i = 0; i < s.Length - buffersize; i++) {
-        bool contains = HasDuplicates(s[i..(i + buffersize)]);
-        if (!contains) return i + buffersize;
+        if (!HasDuplicateCharacters(s[i..(i + buffersize)])) return i + buffersize;
     }
-	return -1;
+    return -1;
 }
 
-bool HasDuplicates(string s) {
-	for (int i = 0; i < s.Length; i++) {
-		for (int j = i+1; j < s.Length; j++) {
-			if (s[i] == s[j]) return true;
-		}
-	}
-	return false;
+bool HasDuplicateCharacters(string s) {
+    for (int i = 0; i < s.Length; i++) {
+        for (int j = i + 1; j < s.Length; j++) {
+            if (s[i] == s[j]) return true;
+        }
+    }
+    return false;
 }

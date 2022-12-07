398ed421752837fca4c17b2d2305504ad7345abd
Christoph Neumüller <c.neumueller@gmail.com>
2022-12-07 18:15:36 +0100

    07 part 1

---
+++ 2022/07/Program.cs
@@ -0,0 +1,54 @@
+﻿using utils;
+
+using var r = new MyReader(File.OpenText("input.txt"));
+
+var dirsizes = new Dictionary<string, int>();
+var dirstack = new Stack<string>();
+
+string curdir = "";
+string listdir = "";
+while (!r.EOF) {
+    var line = r.ReadLine();
+    if (line.StartsWith("$")) listdir = "";
+
+    if (line == "$ cd ..") {
+        curdir = ExitDir(dirstack);
+    } else if (line.StartsWith("$ cd")) {
+        var dir = line.Split(' ')[2];
+        curdir = EnterDir(dir, curdir, dirstack);
+        if (!dirsizes.ContainsKey(curdir)) dirsizes.Add(curdir, 0);
+    }  else if (line.StartsWith("dir ")) {
+        if (listdir != "") {
+            dirsizes.Add(listdir + line.Split(' ')[1] + "/", 0);
+        }
+    } else if (char.IsDigit(line[0])) {
+        if (listdir != "") {
+            foreach (var key in dirsizes.Keys) {
+                if (listdir.StartsWith(key)) dirsizes[key] += int.Parse(line.Split(' ')[0]);
+            }
+            //dirsizes[listdir] += int.Parse(line.Split(' ')[0]);
+        }
+    } else if (line == "$ ls") {
+        listdir = curdir;
+    }
+}
+
+int sum = 0;
+foreach (var item in dirsizes) {
+    Console.WriteLine(item);
+    if (item.Value <= 100_000) sum += item.Value;
+}
+Console.WriteLine(sum);
+
+// 572069967 is too high!
+
+string ExitDir(Stack<string> dirstack) {
+    dirstack.Pop();
+    return dirstack.Peek();
+}
+
+string EnterDir(string dir, string curdir, Stack<string> dirstack) {
+    var newdir = dir.StartsWith("/") ? dir : curdir + dir + "/";
+    dirstack.Push(newdir);
+    return newdir;
+}

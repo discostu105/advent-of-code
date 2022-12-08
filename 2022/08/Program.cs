cc49029c7d2f23b890887dbbfe81faba4a116ffb
Christoph Neumüller <c.neumueller@gmail.com>
2022-12-08 13:02:05 +0100

    beautify

--- 2022/08/Program.cs
+++ 2022/08/Program.cs
@@ -1,4 +1,5 @@
-﻿using utils;
+﻿using System.Diagnostics;
+using utils;
 
 using var r = new MyReader(File.OpenText("input.txt"));
 
@@ -15,70 +16,46 @@ int visibleTrees = 0;
 int maxTreesInSight = 0;
 for (int row = 0; row < forrest.Count; row++) {
     for (int col = 0; col < forrest[row].Count; col++) {
-        bool isVisible = IsVisible(forrest, row, col);
-        if (isVisible) visibleTrees++;
-        int treesInSight = CountVisibleTrees(forrest, row, col);
+        CountVisibleTrees(forrest, row, col, out var isVisibleFromOutside, out var treesInSight);
+        if (isVisibleFromOutside) visibleTrees++;
         maxTreesInSight = Math.Max(maxTreesInSight, treesInSight);
-        Console.WriteLine($"row={row},col={col}: {forrest[row][col]}, isVisible: {isVisible}, treesInSight: {treesInSight}");
+        Console.WriteLine($"row={row},col={col}: {forrest[row][col]}, isVisibleFromOutside: {isVisibleFromOutside}, treesInSight: {treesInSight}");
     }
 }
 
 Console.WriteLine("part 1: " + visibleTrees);
 Console.WriteLine("part 2: " + maxTreesInSight);
 
-static bool IsVisible(List<List<int>> forrest, int row, int col) {
-    bool left, right, top, bottom;
-    left = right = top = bottom = true;
-    
-    // visible from left (search columns leftward)
-    for (int compareCol = col - 1; compareCol >= 0; compareCol--) {
-        if (forrest[row][col] <= forrest[row][compareCol]) left = false;
-    }
-
-    // visible from right (search columns rightward)
-    for (int compareCol = col + 1; compareCol < forrest[row].Count; compareCol++) {
-        if (forrest[row][col] <= forrest[row][compareCol]) right = false;
-    }
-
-    // visible from top (search row upward)
-    for (int compareRow = row - 1; compareRow >= 0; compareRow--) {
-        if (forrest[row][col] <= forrest[compareRow][col]) top = false;
-    }
-
-    // visible from bottom (search row downward)
-    for (int compareRow = row + 1; compareRow < forrest.Count; compareRow++) {
-        if (forrest[row][col] <= forrest[compareRow][col]) bottom = false;
-    }
-    return left || right || top || bottom;
-}
-
-
-static int CountVisibleTrees(List<List<int>> forrest, int row, int col) {
+static void CountVisibleTrees(List<List<int>> forrest, int row, int col, out bool isVisibleFromOutside, out int treesInSight) {
     int left, right, top, bottom;
     left = right = top = bottom = 0;
+
+    bool vleft, vright, vtop, vbottom;
+    vleft = vright = vtop = vbottom = true;
 
     // visible from left (search columns leftward)
     for (int compareCol = col - 1; compareCol >= 0; compareCol--) {
         if (forrest[row][col] > forrest[row][compareCol]) left++;
-        if (forrest[row][col] <= forrest[row][compareCol]) { left++; break; }
+        if (forrest[row][col] <= forrest[row][compareCol]) { left++; vleft &= false; break; }
     }
 
     // visible from right (search columns rightward)
     for (int compareCol = col + 1; compareCol < forrest[row].Count; compareCol++) {
         if (forrest[row][col] > forrest[row][compareCol]) right++;
-        if (forrest[row][col] <= forrest[row][compareCol]) { right++; break; }
+        if (forrest[row][col] <= forrest[row][compareCol]) { right++; vright &= false; break; }
     }
 
     // visible from top (search row upward)
     for (int compareRow = row - 1; compareRow >= 0; compareRow--) {
         if (forrest[row][col] > forrest[compareRow][col]) top++;
-        if (forrest[row][col] <= forrest[compareRow][col]) { top++; break; }
+        if (forrest[row][col] <= forrest[compareRow][col]) { top++; vtop &= false; break; }
     }
 
     // visible from bottom (search row downward)
     for (int compareRow = row + 1; compareRow < forrest.Count; compareRow++) {
         if (forrest[row][col] > forrest[compareRow][col]) bottom++;
-        if (forrest[row][col] <= forrest[compareRow][col]) { bottom++; break; }
+        if (forrest[row][col] <= forrest[compareRow][col]) { bottom++; vbottom &= false; break; }
     }
-    return left * right * top * bottom;
+    isVisibleFromOutside = vleft || vright || vtop || vbottom;
+    treesInSight = left * right * top * bottom;
 }

fa691c4bf10ac32b3db45d53b08a9aec4e80aa76
Christoph Neumüller <c.neumueller@gmail.com>
2022-12-17 21:19:28 +0100

    17 opimizations

--- 2022/17/Program.cs
+++ 2022/17/Program.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 var input = File.ReadAllText("input.txt");
 
-Console.WriteLine(Simulate(input, 2022));
+//Console.WriteLine(Simulate(input, 2022));
 Console.WriteLine(Simulate(input, 1000000000000));
 
 int Simulate(string moves, long numberOfShapes) {
@@ -14,7 +14,7 @@ int Simulate(string moves, long numberOfShapes) {
     var cave = new Cave(7);
     var sw = new Stopwatch();
     sw.Start();
-    int reportInterval = 10000;
+    int reportInterval = 100000;
 
     var movePos = 0;
     var shapes = EnumerateShapes().GetEnumerator();
@@ -22,22 +22,22 @@ int Simulate(string moves, long numberOfShapes) {
         shapes.MoveNext();
         var shape = shapes.Current;
         cave.SpawnShape(shape);
-        //cave.Print();
+        //cave.Print(shape);
         while (true) {
             cave.TryPush(shape, moves[movePos++]);
             movePos %= maxMoves;
-            //cave.Print();
+            //cave.Print(shape);
             if (!cave.TryFall(shape)) break;
-            //cave.Print();
+            //cave.Print(shape);
         }
-        //cave.Print();
-        if (i % 100 == 0) cave.Optimize();
-        //cave.Print();
-        if (i % reportInterval == 0) {
+        //cave.Print(shape);
+        cave.FixateShape(shape);
+        //cave.Print(shape);
+        if (i % reportInterval == 0 || i == numberOfShapes - 1) {
             var elapsed = sw.Elapsed;
             sw.Restart();
             var remaining = TimeSpan.FromSeconds(elapsed.TotalSeconds * ((double)numberOfShapes / reportInterval));
-            Console.WriteLine($"At block {i}, height is {cave.Height}. Took {elapsed}. Remaining time: {remaining}.");
+            Console.WriteLine($"At block {i}, height is {cave.Height}. Took {elapsed}. Remaining time: {remaining}. rows: {cave.CaveRows.Count}");
 
         }
     }
@@ -61,41 +61,43 @@ class Shape {
     public char Symbol { get; init; }
     //public string[] Sprite { get; init; }
 
-    private Dictionary<int, BitArray> Rows = new Dictionary<int, BitArray>();
-    public int Width { get => Rows[0].Length; }
-    public int Height { get => Rows.Coun; }
+    public Dictionary<int, BitArray> Rows = new Dictionary<int, BitArray>();
+    public int Width { get; set; }
+    public int Height { get; set; }
 
-    public Shape(char symbol, string[] sprite) {
+    public Shape(char symbol, Dictionary<int, BitArray> rows) {
         Symbol = symbol;
-        Sprite = sprite;
+        Rows = rows;
+        Width = rows[0].Length;
+        Height = rows.Count;
     }
 
-    internal static Shape NewPlus() => new Shape('+', new[] {
-        ".#.",
-        "###",
-        ".#.",
+    internal static Shape NewPlus() => new Shape('+', new Dictionary<int, BitArray> {
+        [0] = new BitArray(new bool[] { false, true, false }),
+        [1] = new BitArray(new bool[] { true, true, true }),
+        [2] = new BitArray(new bool[] { false, true, false }),
     });
 
-    internal static Shape NewDash() => new Shape('@', new[] {
-        "####",
+    internal static Shape NewDash() => new Shape('@', new Dictionary<int, BitArray> {
+        [0] = new BitArray(new bool[] { true, true, true, true }),
     });
 
-    internal static Shape NewL() => new Shape('L', new[] {
-        "###",
-        "..#",
-        "..#",
+    internal static Shape NewL() => new Shape('L', new Dictionary<int, BitArray> {
+        [0] = new BitArray(new bool[] { true, true, true }),
+        [1] = new BitArray(new bool[] { false, false, true }),
+        [2] = new BitArray(new bool[] { false, false, true }),
     });
 
-    internal static Shape NewI() => new Shape('I', new[] {
-        "#",
-        "#",
-        "#",
-        "#",
+    internal static Shape NewI() => new Shape('I', new Dictionary<int, BitArray> {
+        [0] = new BitArray(new bool[] { true }),
+        [1] = new BitArray(new bool[] { true }),
+        [2] = new BitArray(new bool[] { true }),
+        [3] = new BitArray(new bool[] { true }),
     });
 
-    internal static Shape NewSquare() => new Shape('O', new[] {
-        "##",
-        "##",
+    internal static Shape NewSquare() => new Shape('O', new Dictionary<int, BitArray> {
+        [0] = new BitArray(new bool[] { true, true }),
+        [1] = new BitArray(new bool[] { true, true }),
     });
 
     public bool HitLocal(Point coord) {
@@ -111,86 +113,36 @@ class Shape {
 
 class Cave {
     private int width;
-    private List<Shape> shapes = new List<Shape>();
-    private Dictionary<int, BitArray> rows = new Dictionary<int, BitArray>();
+    public Dictionary<int, BitArray> CaveRows = new Dictionary<int, BitArray>();
+    private int maxY = 0;
 
-    public int Height { get => shapes.Max(s => s.CaveCoordinates.Y + s.Height) + heightOffset; }
+    public int Height { get => maxY; }
 
     public Cave(int width) {
         this.width = width;
     }
 
-    // remove all shapes. instead initialize a matrix of already blocked coordinates
-    // if a line is fully blocked, assume everything below is "unreachable", so does not need to be stored ("heightOffset")
-    internal void Optimize() {
-        //var sprite = new List<char[]>();
-
-        //for (int y = Height; y >= 0; y--) {
-        //    var line = new char[width];
-        //    for (int x = 0; x < width; x++) {
-        //        if (!IsFree(null, new Point(x, y))) {
-        //            line[x] = '#';
-        //        } else {
-        //            line[x] = '.';
-        //        }
-        //    }
-        //    sprite.Add(line);
-        //    if (line.All(x => x == '#')) {
-        //        //Console.WriteLine(" ---- CUTOFF ---- ");
-        //        break;
-        //    }
-        //}
-        //sprite.Reverse();
-        var blocked = new HashSet<Point>();
-        //var mainshape = shapes.SingleOrDefault(x => x.Symbol == '#');
-        foreach(var shape in shapes) {
-            //if (shape == mainshape) continue;
-            for (int x = 0; x < shape.Width; x++) {
-                for (int y = 0; y < shape.Height; y++) {
-                    if (shape.HitLocal(new Point(x, y))) blocked.Add(new Point(x + shape.CaveCoordinates.X, y + shape.CaveCoordinates.Y));
+    internal void FixateShape(Shape shape) {
+        foreach(var row in shape.Rows) {
+            var y = shape.CaveCoordinates.Y + row.Key;
+            if (!CaveRows.ContainsKey(y)) {
+                CaveRows.Add(y, new BitArray(7, false));
+            }
+            for (int x = 0; x < row.Value.Count; x++) {
+                CaveRows[y][shape.CaveCoordinates.X + x] = row.Value[x];
+            }
+            if (CaveRows[y].Cast<bool>().All(x => x)) {
+                //Console.WriteLine("removing rows below " + y);
+                foreach (var rowToRemove in CaveRows.Keys.Where(row => row < y).ToList()) {
+                    CaveRows.Remove(rowToRemove);
                 }
             }
         }
-
-        var maxX = blocked.Max(coord => coord.X);
-        var minX = blocked.Min(coord => coord.X);
-        var maxY = blocked.Max(coord => coord.Y);
-        var minY = blocked.Min(coord => coord.Y);
-
-        if (maxY - minY < 0) return;
-
-        var sprite = new string[maxY - minY + 1];
-        for (int y = minY; y < maxY - minY + 1; y++) {
-            var sb = new StringBuilder(maxX + 1);
-            for (int x = 0; x < width; x++) {
-                sb.Append(blocked.Contains(new Point(x, y)) ? '#' : '.');
-            }
-            sprite[y] = sb.ToString();
-        }
-
-        shapes = new List<Shape>();
-        shapes.Add(new Shape('#', sprite.Select(x => new string(x)).ToArray()));
-        //if (mainshape != null) shapes.Add(mainshape);
-
-        //var blocked = new List<bool[]>();
-        //for (int y = Height; y >= 0; y--) {
-        //    var row = new bool[width];
-        //    for (int x = 0; x < width; x++) {
-        //        if (!IsFree(null, new Point(x, y))) row[x] = true;
-        //    }
-        //    blocked.Add(row);
-        //    if (row.All(x=>x)) {
-        //        heightOffset = y;
-        //        break;
-        //    }
-        //}
-        //this.blocked = blocked;
-        //this.shapes = new List<Shape>(); // clear shapes
+        maxY = Math.Max(maxY, shape.CaveCoordinates.Y + shape.Height);
     }
 
     internal void SpawnShape(Shape shape) {
         shape.CaveCoordinates = FindSpawnPosition(shape);
-        shapes.Add(shape);
     }
 
     internal bool TryFall(Shape shape) {
@@ -207,22 +159,17 @@ class Cave {
         if (fallCoordinates.X < 0 || fallCoordinates.X + shape.Width > width) return false;
         if (fallCoordinates.Y < 0) return false;
         //if (HitBlocked(fallCoordinates)) return false;
-        var movedShape = new Shape(shape.Symbol, shape.Sprite) {
+        var movedShape = new Shape(shape.Symbol, shape.Rows) {
             CaveCoordinates = fallCoordinates
         };
         for (int y = 0; y < shape.Height; y++) {
             for (int x = 0; x < shape.Width; x++) {
-                if (shape.HitLocal(new Point(x, y)) && !this.IsFree(shape, new Point(movedShape.CaveCoordinates.X + x, movedShape.CaveCoordinates.Y + y)))
+                if (shape.HitLocal(new Point(x, y)) && !this.IsFree(new Point(movedShape.CaveCoordinates.X + x, movedShape.CaveCoordinates.Y + y)))
                     return false;
             }
         }
         return true;
     }
-
-    //private bool HitBlocked(Point fallCoordinates) {
-    //    if (fallCoordinates.Y - heightOffset >= blocked.Count) return false;
-    //    return blocked[fallCoordinates.Y - heightOffset][fallCoordinates.X];
-    //}
 
     internal bool TryPush(Shape shape, char direction) {
         //Console.WriteLine("Pushing towards " + direction);
@@ -240,19 +187,21 @@ class Cave {
     }
 
     private Point FindSpawnPosition(Shape shape) {
-        if (shapes.Count == 0) return new Point(2, 3);
-        return new Point(2, shapes.Max(s => s.CaveCoordinates.Y + s.Height) + 3);
+        if (CaveRows.Count == 0) return new Point(2, 3);
+        return new Point(2, maxY + 3);
     }
 
-    internal void Print() {
-        for (int y = Height - 1; y >= 0; y--) {
+    internal void Print(Shape shape = null) {
+        var height = shape == null ? this.Height : Math.Max(this.Height, shape.CaveCoordinates.Y + shape.Height);
+        for (int y = height - 1; y >= 0; y--) {
             Console.Write('|');
             for (int x = 0; x < width; x++) {
-                var shape = FindShapeAtPosition(new Point(x, y));
-                if (shape == null) {
-                    Console.Write('.');
-                } else {
+                if (shape != null && shape.HitGlobal(new Point(x, y))) {
                     Console.Write(shape.Symbol);
+                } else if (!IsFree(new Point(x, y))) {
+                    Console.Write('#');
+                } else {
+                    Console.Write('.');
                 }
             }
             Console.Write('|');
@@ -266,14 +215,10 @@ class Cave {
         Console.WriteLine();
     }
 
-    private Shape? FindShapeAtPosition(Point point) {
-        return shapes.SingleOrDefault(s => s.HitGlobal(point));
-    }
-
-    private bool IsFree(Shape excludeShape, Point point) {
-        var shapeFound = FindShapeAtPosition(point);
-        if (shapeFound == null) return true;
-        if (shapeFound == excludeShape) return true;
-        return false;
+    private bool IsFree(Point point) {
+        if (CaveRows.TryGetValue(point.Y, out var caveRow)) {
+            return !caveRow[point.X];
+        }
+        return true;
     }
 }
